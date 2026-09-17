2d24bb58a636eeb2e2bf020646a8b2060de37b1c
Sergivaldo Clemente Gomes Filho <sergiccb@gmail.com>
2026-09-16 22:47:59 -0300

    fix: corrige a sintaxe do bloco de configuração do provider helm

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -18,7 +18,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
     client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
