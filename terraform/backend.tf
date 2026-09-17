e92ffddc6a0d52b0f828eb0ab24720f91ef856c3
Sergivaldo Clemente Gomes Filho <sergiccb@gmail.com>
2026-09-16 22:30:26 -0300

    fix: altera versão base dos providers terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.80"
+      version = "~> 5.5"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.12"
+      version = "~> 3.3"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.24"
+      version = "~> 3.2"
     }
   }
 
